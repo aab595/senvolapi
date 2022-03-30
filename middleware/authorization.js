1f97ec532b0443d09cffea76adc0dbbc5c1eec41
Abdoul Aziz BA <aaziz.ba2@univ-thies.sn>
2022-03-30 14:33:02 +0000

    Manage token

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 const verifyToken = (req, res, next) => {
-	const authHeader = req.headers.token;
+	const authHeader = req.headers.authorization;
 	if (authHeader) {
 		const token = authHeader.split(" ")[1];
 		jwt.verify(token, process.env.JWT_SEC_KEY, (err, user) => {
